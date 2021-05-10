8db6cbcabe14ceed2436dec4a04031e47264fdbc
romanbatavi <romanbatavi98@gmail.com>
2021-05-11 00:17:16 +0700

    globacl style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router} from 'react-router-dom'
+import { GlobalStyle } from './GlobalStyle';
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <Navbar />
     </Router>
   );
