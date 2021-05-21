c049aa6a03bf8caa860f70b3f2794e2705eef717
romanbatavi <romanbatavi98@gmail.com>
2021-05-21 18:50:19 +0700

    product element

--- src/components/Products/ProductsElements.js
+++ src/components/Products/ProductsElements.js
@@ -7,6 +7,38 @@ export const ProductsContainer = styled.div`
   background: #150f0f;
   color: #fff;
 `
+
 export const ProductsWrapper = styled.div`
-  
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 0 auto;
+`
+
+export const ProductCard = styled.div`
+  margin: 0;
+  line-height: 2;
+  width: 300px;
+`
+
+export const ProductImg = styled.div`
+  height: 300px;
+  min-width: 300px;
+  max-width: 100%;
+  box-shadow: 8px 8px #fdc500;
+`
+
+export const ProductHeading = styled.h1`
+  font-size: clamp(2rem, 2.5vw, 3rem);
+  text-align: center;
+  font-size: 1.5rem;
+`
+
+export const ProductTitle = styled.h2`
+  font-weight: 400;
+  font-size: 1.5rem;
+` 
+
+export const ProductInfo = styled.div`
+  display: flex;
 `
