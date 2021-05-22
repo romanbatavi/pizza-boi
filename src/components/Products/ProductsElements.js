f17f502bdb70eb008cad35906a8d98ac7d245079
romanbatavi <romanbatavi98@gmail.com>
2021-05-23 00:34:57 +0700

    productbutton finish

--- src/components/Products/ProductsElements.js
+++ src/components/Products/ProductsElements.js
@@ -61,4 +61,14 @@ export const ProductButton = styled.button`
   font-size: 1rem;
   padding: 1rem 4rem;
   border: none;
-`
+  background: #e31837;
+  color: #fff;
+  transition: 0.2 ease-out;
+
+  &:hover {
+    background: #ffc500;
+    transition: 0.2s ease-out;
+    cursor: pointer;
+    color: #000;
+  }
+`;
